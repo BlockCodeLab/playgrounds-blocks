6a5b6261a484ccd298d8370d34c33ffc2fcbfb9f
Yeqin Gong <gon@gyeq.in>
2025-10-24 17:30:24 +0800

    添加DHT温湿度传感器

---
+++ extensions/dht/src/info.jsx
@@ -0,0 +1,37 @@
+import { addLocalesMessages, Text } from '@blockcode/core';
+import featureImage from './feature.svg';
+import iconImage from './icon.svg';
+
+export default {
+  beta: true,
+  image: featureImage,
+  icon: iconImage,
+  name: (
+    <Text
+      id="blocks.dht.name"
+      defaultMessage="DHT"
+    />
+  ),
+  description: (
+    <Text
+      id="blocks.dht.description"
+      defaultMessage="DHT11/DHT22 Temperature and humidity."
+    />
+  ),
+  tags: ['device', 'arduino', 'sensor'],
+};
+
+addLocalesMessages({
+  en: {
+    'blocks.dht.name': 'DHT',
+    'blocks.dht.description': 'DHT11/DHT22 Temperature and humidity.',
+  },
+  'zh-Hans': {
+    'blocks.dht.name': 'DHT 温湿度',
+    'blocks.dht.description': '使用 DHT11/DHT22 的温湿度传感器。',
+  },
+  'zh-Hant': {
+    'blocks.dht.name': 'DHT 溫濕度',
+    'blocks.dht.description': '使用 DHT11/DHT22 的溫濕度傳感器。',
+  },
+});
