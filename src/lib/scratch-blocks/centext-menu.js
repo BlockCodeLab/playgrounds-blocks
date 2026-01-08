34d97c7338b3074d6161f59c5156a1e16cf49a75
Yeqin Gong <yeqin.gong@qq.com>
2026-01-08 10:27:00 +0800

    优化积木图片导出

--- src/lib/scratch-blocks/centext-menu.js
+++ src/lib/scratch-blocks/centext-menu.js
@@ -1,6 +1,29 @@
-import { saveSvgAsPng, nanoid } from '@blockcode/utils';
-import { translate } from '@blockcode/core';
+import { svgAsPngUri, dataURItoBlob, exportFile, nanoid } from '@blockcode/utils';
+import { translate, setAlert } from '@blockcode/core';
 import { ScratchBlocks } from './scratch-blocks';
+
+const saveDataUri = (uri) => {
+  const data = dataURItoBlob(uri);
+  return exportFile(data, `${nanoid()}.png`)
+    .then(() =>
+      setAlert(
+        {
+          type: 'success',
+          message: translate('blocks.centextMenu.exportPngDone', 'PNG exported.'),
+        },
+        2000,
+      ),
+    )
+    .catch(() =>
+      setAlert(
+        {
+          type: 'warn',
+          message: translate('blocks.centextMenu.exportPngFailed', 'Failed to export image.'),
+        },
+        2000,
+      ),
+    );
+};
 
 // 给工作区添加导出积木图片菜单项
 const showContextMenu = ScratchBlocks.ContextMenu.show;
@@ -20,7 +43,17 @@ ScratchBlocks.ContextMenu.show = function (e, options, rtl) {
       const workspace = ScratchBlocks.getMainWorkspace();
       const canvas = workspace.getCanvas();
       if (canvas) {
-        saveSvgAsPng(canvas, nanoid());
+        svgAsPngUri(canvas)
+          .then(saveDataUri)
+          .catch(() => {
+            setAlert(
+              {
+                type: 'warn',
+                message: translate('blocks.centextMenu.exportPngFailed', 'Failed to export image.'),
+              },
+              2000,
+            );
+          });
       }
     };
   } else {
@@ -28,18 +61,32 @@ ScratchBlocks.ContextMenu.show = function (e, options, rtl) {
     exportPngOption.callback = () => {
       const block = ScratchBlocks.ContextMenu.currentBlock;
       const options = {};
-      const bbox = block.svgGroup_.getBBox();
+      const getBBox = block.svgGroup_.getBBox.bind(block.svgGroup_);
+      const bbox = getBBox();
       // 修复帽子积木被裁剪掉帽子
       bbox.y = 0;
       bbox.width += 2;
       bbox.height += 1;
-      block.svgGroup_.getBBox = () => bbox;
       if (block.startHat_) {
         options.top = -22;
         options.left = -1;
         bbox.height += 5;
       }
-      saveSvgAsPng(block.svgGroup_, nanoid(), options);
+      block.svgGroup_.getBBox = () => bbox;
+      svgAsPngUri(block.svgGroup_, options)
+        .then((uri) => {
+          block.svgGroup_.getBBox = getBBox;
+          saveDataUri(uri);
+        })
+        .catch(() => {
+          setAlert(
+            {
+              type: 'warn',
+              message: translate('blocks.centextMenu.exportPngFailed', 'Failed to export image.'),
+            },
+            2000,
+          );
+        });
     };
   }
 
