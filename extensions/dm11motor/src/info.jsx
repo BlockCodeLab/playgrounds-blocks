40d3a91e57c466767482e2b5a5261d521abec4c8
Yeqin Gong <yeqin.gong@qq.com>
2026-01-12 17:42:49 +0800

    更新文本和图片

--- extensions/dm11motor/src/info.jsx
+++ extensions/dm11motor/src/info.jsx
@@ -4,13 +4,13 @@ import featureImage from './feature.svg';
 import iconImage from './icon.svg';
 
 export default {
-  beta: true,
+  preview: true,
   image: featureImage,
   icon: iconImage,
   name: (
     <Text
       id="blocks.dm11motor.name"
-      defaultMessage="DM11 Motor"
+      defaultMessage="DM11 Motor Board"
     />
   ),
   description: (
@@ -31,17 +31,17 @@ export default {
 
 addLocalesMessages({
   en: {
-    'blocks.dm11motor.name': 'DM11 Motor',
-    'blocks.dm11motor.description': 'DM11 dual motor driver.',
+    'blocks.dm11motor.name': 'DM11 Motor Board',
+    'blocks.dm11motor.description': 'DM11 dual motor driver board.',
     'blocks.dm11motor.collaborator': 'Emakefun',
   },
   'zh-Hans': {
-    'blocks.dm11motor.name': 'DM11 电机驱动',
+    'blocks.dm11motor.name': 'DM11 电机驱动板',
     'blocks.dm11motor.description': 'DM11 双电机驱动板。',
     'blocks.dm11motor.collaborator': '易创空间',
   },
   'zh-Hant': {
-    'blocks.dm11motor.name': 'DM11 電機驅動',
+    'blocks.dm11motor.name': 'DM11 電機驅動板',
     'blocks.dm11motor.description': 'DM11 雙電機驅動板。',
     'blocks.dm11motor.collaborator': 'Emakefun',
   },
