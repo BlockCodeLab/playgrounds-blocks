2971e66c9402cb3ac1d5c787e7ae07af81d2879c
Yeqin Gong <yeqin.gong@qq.com>
2026-02-13 20:51:45 +0800

    优化文本，修复不同编辑器案例加载bug

--- extensions/aivoxassistant/src/lessons.jsx
+++ extensions/aivoxassistant/src/lessons.jsx
@@ -1,7 +1,9 @@
 import { Text } from '@blockcode/core';
 import feature from './feature.svg';
 
-import exmple from './assets/aivoxassistant.bcp';
+import exmpleArduino from './assets/aivoxassistant-arduino.bcp';
+import exmpleEsp32 from './assets/aivoxassistant-esp32.bcp';
+import exmpleIotBit from './assets/aivoxassistant-iotbit.bcp';
 import knowledge from './assets/knowledge.png';
 import xiaozhi from './assets/xiaozhi.png';
 
@@ -10,24 +12,35 @@ export const lessons = {
     title: (
       <Text
         id="blocks.aivoxassistant.lessonTitle"
-        defaultMessage="AI-Vox Assistant Help"
+        defaultMessage="AI Vox Assistant Help"
       />
     ),
     image: feature,
-    project: exmple,
+    project(meta) {
+      if (meta.editor === '@blockcode/gui-arduino') {
+        return exmpleArduino;
+      }
+      if (meta.editor === '@blockcode/gui-esp32') {
+        return exmpleEsp32;
+      }
+      if (meta.editor === '@emakefun/gui-iotbit') {
+        return exmpleIotBit;
+      }
+      return;
+    },
     pages: [
       {
         title: (
           <Text
             id="blocks.aivoxassistant.lessonPage1.title"
-            defaultMessage="AI-Vox Assistant Help"
+            defaultMessage="Peripheral Control Commands"
           />
         ),
         image: knowledge,
         text: (
           <Text
             id="blocks.aivoxassistant.lessonPage1.text"
-            defaultMessage="AI-Vox Assistant Help"
+            defaultMessage="Create a new Excel file, enter the peripheral control commands for this example in the format shown above, and then save it."
           />
         ),
       },
@@ -35,14 +48,14 @@ export const lessons = {
         title: (
           <Text
             id="blocks.aivoxassistant.lessonPage2.title"
-            defaultMessage="AI-Vox Assistant Help"
+            defaultMessage="Import to Xiaozhi Knowledge Base"
           />
         ),
         image: xiaozhi,
         text: (
           <Text
             id="blocks.aivoxassistant.lessonPage2.text"
-            defaultMessage="AI-Vox Assistant Help"
+            defaultMessage='Import the "Peripheral Control Commands" into the knowledge base of the Xiaozhi backend at xiaozhi.me.'
           />
         ),
       },
