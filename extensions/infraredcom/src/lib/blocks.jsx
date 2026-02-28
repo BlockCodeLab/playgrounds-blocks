60a1faaff636ccf95527703c9c3981a123837528
Yeqin Gong <yeqin.gong@qq.com>
2026-02-28 17:27:59 +0800

    更新事件轮询，添加事件轮询积木

--- extensions/infraredcom/src/lib/blocks.jsx
+++ extensions/infraredcom/src/lib/blocks.jsx
@@ -1,209 +1,223 @@
 import { Text } from '@blockcode/core';
 
-export const blocks = (meta) => [
-  {
-    id: 'initReceive',
-    text: (
-      <Text
-        id="blocks.infraredcom.initReceive"
-        defaultMessage="set pin [PIN] to ir receiver"
-      />
-    ),
-    inputs: {
-      PIN: meta.boardPins
-        ? {
-            menu: meta.boardPins.in,
-          }
-        : {
-            type: 'positive_integer',
-            defaultValue: '1',
-          },
+export const blocks = (meta) =>
+  [
+    {
+      id: 'initReceive',
+      text: (
+        <Text
+          id="blocks.infraredcom.initReceive"
+          defaultMessage="set pin [PIN] to ir receiver"
+        />
+      ),
+      inputs: {
+        PIN: meta.boardPins
+          ? {
+              menu: meta.boardPins.in,
+            }
+          : {
+              type: 'positive_integer',
+              defaultValue: '1',
+            },
+      },
+      ino(block) {
+        const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
+        this.definitions_['setup_irreceiver'] = `IrReceiver.begin(${pin});`;
+        return '';
+      },
     },
-    ino(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
-      this.definitions_['setup_irreceiver'] = `IrReceiver.begin(${pin});`;
-      return '';
+    meta.editor === '@blockcode/gui-arduino' && {
+      id: 'eventPolling',
+      text: (
+        <Text
+          id="blocks.infraredcom.eventPolling"
+          defaultMessage="event polling"
+        />
+      ),
+      ino(block) {
+        const code = 'irreceiver_received();\n';
+        return code;
+      },
     },
-  },
-  {
-    id: 'whenReceived',
-    text: (
-      <Text
-        id="blocks.infraredcom.whenReceived"
-        defaultMessage="when ir received"
-      />
-    ),
-    hat: true,
-    ino(block) {
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
+    '---',
+    {
+      id: 'whenReceived',
+      text: (
+        <Text
+          id="blocks.infraredcom.whenReceived"
+          defaultMessage="when ir received"
+        />
+      ),
+      hat: true,
+      ino(block) {
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
 
-      // 加入事件定时器
-      const funcName = `irreceiver_whenreceived`;
-      this.definitions_['tick_irreceiver'] = `${funcName}();`;
-      this.definitions_['declare_irreceiver'] = `void ${funcName}();`;
+        // 加入事件定时器
+        const funcName = 'irreceiver_received';
+        this.definitions_['declare_irreceiver'] = `void ${funcName}();`;
 
-      const branchCode = this.statementToCode(block) || '';
-      let code = '';
-      code += `void ${funcName}() {\n`;
-      code += '  bool received = IrReceiver.decode();\n';
-      code += '  IrReceiver.resume();\n';
-      code += '  if (!received || IrReceiver.decodedIRData.protocol == UNKNOWN) return;\n';
-      code += `${branchCode}}\n`;
-      this.definitions_[funcName] = code;
+        const branchCode = this.statementToCode(block) || '';
+        let code = '';
+        code += `void ${funcName}() {\n`;
+        code += '  bool received = IrReceiver.decode();\n';
+        code += '  IrReceiver.resume();\n';
+        code += '  if (!received || IrReceiver.decodedIRData.protocol == UNKNOWN) return;\n';
+        code += `${branchCode}}\n`;
+        this.definitions_[funcName] = code;
 
-      return '';
-    },
-  },
-  {
-    id: 'receive',
-    text: (
-      <Text
-        id="blocks.infraredcom.receive"
-        defaultMessage="received command"
-      />
-    ),
-    output: 'string',
-    ino(block) {
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
-      const code = 'IrReceiver.decodedIRData.command';
-      return [code, this.ORDER_ATOMIC];
-    },
-  },
-  {
-    id: 'receiveAddr',
-    text: (
-      <Text
-        id="blocks.infraredcom.receiveAddr"
-        defaultMessage="received address"
-      />
-    ),
-    output: 'string',
-    ino(block) {
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
-      const code = 'IrReceiver.decodedIRData.address';
-      return [code, this.ORDER_ATOMIC];
-    },
-  },
-  // {
-  //   id: 'receiveText',
-  //   text: (
-  //     <Text
-  //       id="blocks.infraredcom.receiveText"
-  //       defaultMessage="received String"
-  //     />
-  //   ),
-  //   output: 'string',
-  // },
-  '---',
-  {
-    id: 'initSend',
-    text: (
-      <Text
-        id="blocks.infraredcom.initSend"
-        defaultMessage="set pin [PIN] to ir sender"
-      />
-    ),
-    inputs: {
-      PIN: meta.boardPins
-        ? {
-            menu: meta.boardPins.out,
-          }
-        : {
-            type: 'positive_integer',
-            defaultValue: '1',
-          },
-    },
-    ino(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
-      this.definitions_['setup_irsender'] = `IrSender.begin(${pin});`;
-      return '';
-    },
-  },
-  {
-    id: 'send',
-    text: (
-      <Text
-        id="blocks.infraredcom.send"
-        defaultMessage="send command [CMD] encode [PROTO]"
-      />
-    ),
-    inputs: {
-      CMD: {
-        type: 'hex16',
-        defaultValue: '0',
-      },
-      PROTO: {
-        menu: 'PROTO',
+        return '';
       },
     },
-    ino(block) {
-      const cmd = this.valueToCode(block, 'CMD', this.ORDER_NONE);
-      const proto = block.getFieldValue('PROTO');
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
-      let code = '';
-      code += `IrSender.write(${proto}, 0x0, ${cmd}, 0);\n`;
-      code += 'IrSender.space(20);\n';
-      return code;
-    },
-  },
-  {
-    id: 'sendAddr',
-    text: (
-      <Text
-        id="blocks.infraredcom.sendAddr"
-        defaultMessage="send command:[CMD] address:[ADDR] encode [PROTO]"
-      />
-    ),
-    inputs: {
-      CMD: {
-        type: 'hex16',
-        defaultValue: '0',
-      },
-      ADDR: {
-        type: 'hex16',
-        defaultValue: '0',
-      },
-      PROTO: {
-        menu: 'PROTO',
+    {
+      id: 'receive',
+      text: (
+        <Text
+          id="blocks.infraredcom.receive"
+          defaultMessage="received command"
+        />
+      ),
+      output: 'string',
+      ino(block) {
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
+        const code = 'IrReceiver.decodedIRData.command';
+        return [code, this.ORDER_ATOMIC];
       },
     },
-    ino(block) {
-      const cmd = this.valueToCode(block, 'CMD', this.ORDER_NONE);
-      const addr = this.valueToCode(block, 'ADDR', this.ORDER_NONE);
-      const proto = block.getFieldValue('PROTO');
-      this.definitions_['include_irremote'] = '#include <IRremote.h>';
-      let code = '';
-      code += `IrSender.write(${proto}, ${addr}, ${cmd}, 0);\n`;
-      code += 'IrSender.space(20);\n';
-      return code;
+    {
+      id: 'receiveAddr',
+      text: (
+        <Text
+          id="blocks.infraredcom.receiveAddr"
+          defaultMessage="received address"
+        />
+      ),
+      output: 'string',
+      ino(block) {
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
+        const code = 'IrReceiver.decodedIRData.address';
+        return [code, this.ORDER_ATOMIC];
+      },
     },
-  },
-  // {
-  //   id: 'sendText',
-  //   text: (
-  //     <Text
-  //       id="blocks.infraredcom.sendText"
-  //       defaultMessage="pin [PIN] send String [TEXT]"
-  //     />
-  //   ),
-  //   inputs: {
-  //     PIN: meta.boardPins
-  //       ? {
-  //           menu: meta.boardPins.pwm,
-  //         }
-  //       : {
-  //           type: 'positive_integer',
-  //           defaultValue: '1',
-  //         },
-  //     TEXT: {
-  //       type: 'string',
-  //       defaultValue: 'message',
-  //     },
-  //   },
-  // },
-];
+    // {
+    //   id: 'receiveText',
+    //   text: (
+    //     <Text
+    //       id="blocks.infraredcom.receiveText"
+    //       defaultMessage="received String"
+    //     />
+    //   ),
+    //   output: 'string',
+    // },
+    '---',
+    {
+      id: 'initSend',
+      text: (
+        <Text
+          id="blocks.infraredcom.initSend"
+          defaultMessage="set pin [PIN] to ir sender"
+        />
+      ),
+      inputs: {
+        PIN: meta.boardPins
+          ? {
+              menu: meta.boardPins.out,
+            }
+          : {
+              type: 'positive_integer',
+              defaultValue: '1',
+            },
+      },
+      ino(block) {
+        const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
+        this.definitions_['setup_irsender'] = `IrSender.begin(${pin});`;
+        return '';
+      },
+    },
+    {
+      id: 'send',
+      text: (
+        <Text
+          id="blocks.infraredcom.send"
+          defaultMessage="send command [CMD] encode [PROTO]"
+        />
+      ),
+      inputs: {
+        CMD: {
+          type: 'hex16',
+          defaultValue: '0',
+        },
+        PROTO: {
+          menu: 'PROTO',
+        },
+      },
+      ino(block) {
+        const cmd = this.valueToCode(block, 'CMD', this.ORDER_NONE);
+        const proto = block.getFieldValue('PROTO');
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
+        let code = '';
+        code += `IrSender.write(${proto}, 0x0, ${cmd}, 0);\n`;
+        code += 'IrSender.space(20);\n';
+        return code;
+      },
+    },
+    {
+      id: 'sendAddr',
+      text: (
+        <Text
+          id="blocks.infraredcom.sendAddr"
+          defaultMessage="send command:[CMD] address:[ADDR] encode [PROTO]"
+        />
+      ),
+      inputs: {
+        CMD: {
+          type: 'hex16',
+          defaultValue: '0',
+        },
+        ADDR: {
+          type: 'hex16',
+          defaultValue: '0',
+        },
+        PROTO: {
+          menu: 'PROTO',
+        },
+      },
+      ino(block) {
+        const cmd = this.valueToCode(block, 'CMD', this.ORDER_NONE);
+        const addr = this.valueToCode(block, 'ADDR', this.ORDER_NONE);
+        const proto = block.getFieldValue('PROTO');
+        this.definitions_['include_irremote'] = '#include <IRremote.h>';
+        let code = '';
+        code += `IrSender.write(${proto}, ${addr}, ${cmd}, 0);\n`;
+        code += 'IrSender.space(20);\n';
+        return code;
+      },
+    },
+    // {
+    //   id: 'sendText',
+    //   text: (
+    //     <Text
+    //       id="blocks.infraredcom.sendText"
+    //       defaultMessage="pin [PIN] send String [TEXT]"
+    //     />
+    //   ),
+    //   inputs: {
+    //     PIN: meta.boardPins
+    //       ? {
+    //           menu: meta.boardPins.pwm,
+    //         }
+    //       : {
+    //           type: 'positive_integer',
+    //           defaultValue: '1',
+    //         },
+    //     TEXT: {
+    //       type: 'string',
+    //       defaultValue: 'message',
+    //     },
+    //   },
+    // },
+  ].filter(Boolean);
 
 export const menus = {
   PROTO: {
--- extensions/switchkey/src/blocks.jsx
+++ extensions/switchkey/src/blocks.jsx
@@ -1,128 +1,154 @@
 import { changeCase } from '@blockcode/utils';
 import { Text } from '@blockcode/core';
 
-export const blocks = (meta) => [
-  {
-    id: 'whenKey',
-    text: (
-      <Text
-        id="blocks.switchkey.whenKey"
-        defaultMessage="when pin [PIN] [STATE]"
-      />
-    ),
-    hat: true,
-    inputs: {
-      PIN: meta.boardPins
-        ? { menu: meta.boardPins.in }
-        : {
-            type: 'positive_integer',
-            defaultValue: '1',
-          },
-      STATE: {
-        menu: 'StateOption',
+export const blocks = (meta) =>
+  [
+    meta.editor === '@blockcode/gui-arduino' && {
+      id: 'eventPolling',
+      text: (
+        <Text
+          id="blocks.switchkey.eventPolling"
+          defaultMessage="manual event polling"
+        />
+      ),
+      ino(block) {
+        const funcName = '_1buttonTicks';
+        this.definitions_[`declare_${funcName}`] = `void ${funcName}();`;
+        this.definitions_[funcName] = `void ${funcName}() {\n}`;
+
+        const code = '_1buttonTicks();\n';
+        return code;
       },
     },
-    ino(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      const state = block.getFieldValue('STATE');
-      const buttonName = `_1button_${pin}`;
+    '---',
+    {
+      id: 'whenKey',
+      text: (
+        <Text
+          id="blocks.switchkey.whenKey"
+          defaultMessage="when pin [PIN] [STATE]"
+        />
+      ),
+      hat: true,
+      inputs: {
+        PIN: meta.boardPins
+          ? { menu: meta.boardPins.in }
+          : {
+              type: 'positive_integer',
+              defaultValue: '1',
+            },
+        STATE: {
+          menu: 'StateOption',
+        },
+      },
+      ino(block) {
+        const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+        const state = block.getFieldValue('STATE');
+        const buttonName = `_1button_${pin}`;
 
-      // 加入事件定时器
-      this.definitions_[`tick_${buttonName}`] = `${buttonName}.tick();`;
+        // 加入事件定时器
+        if (this.definitions_['_1buttonTicks']) {
+          this.definitions_['_1buttonTicks'] = this.definitions_['_1buttonTicks'].replace(
+            '\n}',
+            `\n  ${buttonName}.tick();\n}`,
+          );
+        } else {
+          this.definitions_[`tick_${buttonName}`] = `${buttonName}.tick();`;
+        }
 
-      // 绑定按键
-      this.definitions_['include_onebutton'] = '#include <OneButton.h>';
-      this.definitions_[`variable_${buttonName}`] = `OneButton ${buttonName}(${pin});`;
+        // 绑定按键
+        this.definitions_['include_onebutton'] = '#include <OneButton.h>';
+        this.definitions_[`variable_${buttonName}`] = `OneButton ${buttonName}(${pin});`;
 
-      const funcName = `${buttonName}_${state}`;
-      const branchCode = this.statementToCode(block) || '';
-      this.definitions_[`declare_${funcName}`] = `void ${funcName}();`;
-      this.definitions_[funcName] = `void ${funcName}() {\n${branchCode}}`;
-      this.definitions_[`setup_${funcName}`] = `${buttonName}.attach${state}(${funcName});`;
+        const funcName = `${buttonName}_${state}`;
+        const branchCode = this.statementToCode(block) || '';
+        this.definitions_[`declare_${funcName}`] = `void ${funcName}();`;
+        this.definitions_[funcName] = `void ${funcName}() {\n${branchCode}}`;
+        this.definitions_[`setup_${funcName}`] = `${buttonName}.attach${state}(${funcName});`;
+      },
+      mpy(block) {
+        const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+        const state = changeCase.snakeCase(block.getFieldValue('STATE'));
+
+        const buttonName = `_1button_${pin}`;
+        this.definitions_['import_onebutton'] = 'from onebutton import OneButton';
+        this.definitions_[buttonName] = `${buttonName} = OneButton(${pin})`;
+
+        const flagName = this.createName('_1button_flag');
+        this.definitions_[flagName] = `${flagName} = asyncio.ThreadSafeFlag()`;
+
+        // 定义按键回调函数
+        let branchCode = this.statementToCode(block) || '';
+        let code = '';
+        code += 'while True:\n';
+        code += `${this.INDENT}await ${flagName}.wait()\n`;
+        code += branchCode;
+
+        const funcName = this.createName(`${buttonName}_${state}`);
+        branchCode = this.prefixLines(code, this.INDENT);
+        branchCode = this.addEventTrap(branchCode, '1button_callback');
+        code = '@_tasks__.append\n';
+        code += branchCode;
+        code += `${buttonName}.on_${state}(lambda: ${flagName}.set())\n`;
+        this.definitions_[funcName] = code;
+      },
     },
-    mpy(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      const state = changeCase.snakeCase(block.getFieldValue('STATE'));
-
-      const buttonName = `_1button_${pin}`;
-      this.definitions_['import_onebutton'] = 'from onebutton import OneButton';
-      this.definitions_[buttonName] = `${buttonName} = OneButton(${pin})`;
-
-      const flagName = this.createName('_1button_flag');
-      this.definitions_[flagName] = `${flagName} = asyncio.ThreadSafeFlag()`;
-
-      // 定义按键回调函数
-      let branchCode = this.statementToCode(block) || '';
-      let code = '';
-      code += 'while True:\n';
-      code += `${this.INDENT}await ${flagName}.wait()\n`;
-      code += branchCode;
-
-      const funcName = this.createName(`${buttonName}_${state}`);
-      branchCode = this.prefixLines(code, this.INDENT);
-      branchCode = this.addEventTrap(branchCode, '1button_callback');
-      code = '@_tasks__.append\n';
-      code += branchCode;
-      code += `${buttonName}.on_${state}(lambda: ${flagName}.set())\n`;
-      this.definitions_[funcName] = code;
+    {
+      id: 'keyState',
+      text: (
+        <Text
+          id="blocks.switchkey.keyState"
+          defaultMessage="pin [PIN] is pressed?"
+        />
+      ),
+      output: 'boolean',
+      inputs: {
+        PIN: meta.boardPins
+          ? { menu: meta.boardPins.in }
+          : {
+              type: 'positive_integer',
+              defaultValue: '1',
+            },
+      },
+      ino(block) {
+        const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+        const code = `(digitalRead(${pin}) == LOW)`;
+        return [code, this.ORDER_ATOMIC];
+      },
+      mpy(block) {
+        const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+        const buttonName = `_1button_${pin}`;
+        this.definitions_['import_onebutton'] = 'from onebutton import OneButton';
+        this.definitions_[buttonName] = `${buttonName} = OneButton(${pin})`;
+        const code = `${buttonName}.is_pressed()`;
+        return [code, this.ORDER_FUNCTION_CALL];
+      },
     },
-  },
-  {
-    id: 'keyState',
-    text: (
-      <Text
-        id="blocks.switchkey.keyState"
-        defaultMessage="pin [PIN] is pressed?"
-      />
-    ),
-    output: 'boolean',
-    inputs: {
-      PIN: meta.boardPins
-        ? { menu: meta.boardPins.in }
-        : {
-            type: 'positive_integer',
-            defaultValue: '1',
-          },
-    },
-    ino(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      const code = `(digitalRead(${pin}) == LOW)`;
-      return [code, this.ORDER_ATOMIC];
-    },
-    mpy(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      const buttonName = `_1button_${pin}`;
-      this.definitions_['import_onebutton'] = 'from onebutton import OneButton';
-      this.definitions_[buttonName] = `${buttonName} = OneButton(${pin})`;
-      const code = `${buttonName}.is_pressed()`;
-      return [code, this.ORDER_FUNCTION_CALL];
-    },
-  },
-  // '---',
-  // {
-  //   id: 'adcKeyPressed',
-  //   text: (
-  //     <Text
-  //       id="blocks.switchkey.adcKeyPressed"
-  //       defaultMessage="pin [PIN] adc keyboard [KEY] key is pressed?"
-  //     />
-  //   ),
-  //   output: 'boolean',
-  //   inputs: {
-  //     PIN: meta.boardPins
-  //       ? { menu: meta.boardPins.adc }
-  //       : {
-  //           type: 'positive_integer',
-  //           defaultValue: '1',
-  //         },
-  //     KEY: {
-  //       type: 'string',
-  //       defaultValue: 'a',
-  //       menu: ['a', 'b', 'c', 'd', 'e'],
-  //     },
-  //   },
-  // },
-];
+    // '---',
+    // {
+    //   id: 'adcKeyPressed',
+    //   text: (
+    //     <Text
+    //       id="blocks.switchkey.adcKeyPressed"
+    //       defaultMessage="pin [PIN] adc keyboard [KEY] key is pressed?"
+    //     />
+    //   ),
+    //   output: 'boolean',
+    //   inputs: {
+    //     PIN: meta.boardPins
+    //       ? { menu: meta.boardPins.adc }
+    //       : {
+    //           type: 'positive_integer',
+    //           defaultValue: '1',
+    //         },
+    //     KEY: {
+    //       type: 'string',
+    //       defaultValue: 'a',
+    //       menu: ['a', 'b', 'c', 'd', 'e'],
+    //     },
+    //   },
+    // },
+  ].filter(Boolean);
 
 export const menus = {
   StateOption: {
