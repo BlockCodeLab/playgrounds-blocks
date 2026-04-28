54498df5a08187bbce317307d33774ad19c9b989
Yeqin Gong <yeqin.gong@qq.com>
2026-04-28 15:19:54 +0800

    优化案例

--- extensions/c2mqtt/src/info.jsx
+++ extensions/c2mqtt/src/info.jsx
@@ -24,7 +24,7 @@ export default {
       defaultMessage="Emakefun"
     />
   ),
-  tags: ['arduino', 'module'],
+  tags: ['arduino', 'module', 'communication'],
   readme,
 };
 
--- extensions/c2mqtt/src/lessons.jsx
+++ extensions/c2mqtt/src/lessons.jsx
@@ -3,17 +3,19 @@ import feature from './feature.svg';
 
 import exmpleArduino from './assets/esp32c2-mqtt.bcp';
 
+const isArduino = (meta) => ['@blockcode/gui-arduino', '@nulllab/gui-lgtuino'].includes(meta.editor);
+
 export const lessons = {
   'c2mqtt-example': {
     title: (
       <Text
         id="blocks.c2mqtt.lessonTitle"
-        defaultMessage="ESP32C2 MQTT Exmple"
+        defaultMessage="ESP32C2 MQTT Example"
       />
     ),
     image: feature,
     project(meta) {
-      if (meta.editor === '@blockcode/gui-arduino') {
+      if (isArduino(meta)) {
         return exmpleArduino;
       }
     },
