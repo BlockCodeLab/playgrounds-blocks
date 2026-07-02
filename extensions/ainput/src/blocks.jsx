e39aa8320009fc3ca083d79d4a5b17f306c23ea1
Yeqin Gong <yeqin.gong@qq.com>
2026-07-02 18:09:39 +0800

    更新文本描述

--- extensions/ainput/src/blocks.jsx
+++ extensions/ainput/src/blocks.jsx
@@ -78,43 +78,6 @@ export const blocks = (meta) => [
     },
   },
   {
-    id: 'joystick',
-    text: (
-      <Text
-        id="blocks.ainput.joystick"
-        defaultMessage="pin [PIN] joystick"
-      />
-    ),
-    output: 'number',
-    inputs: {
-      PIN: meta.boardPins
-        ? {
-            menu: meta.boardPins.adc,
-          }
-        : {
-            type: 'positive_integer',
-            defaultValue: 1,
-          },
-    },
-    ino(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      this.definitions_[`setup_pin_${pin}`] = `pinMode(${pin}, INPUT);`;
-      const code = `round(((analogRead(${pin}) - 511) / 511.0f) * 100)`;
-      return [code, this.ORDER_FUNCTION_CALL];
-    },
-    mpy(block) {
-      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
-      const pinName = `pin_${pin}`;
-      this.definitions_['import_pin'] = 'from machine import Pin';
-      this.definitions_['import_adc'] = 'from machine import ADC';
-      this.definitions_[pinName] = `${pinName} = ADC(Pin(${pin}))`;
-      this.definitions_[`${pinName}_atten`] = `${pinName}.atten(ADC.ATTN_11DB)`;
-      this.definitions_[`${pinName}_width`] = `${pinName}.width(ADC.WIDTH_10BIT)`;
-      const code = `round(((${pinName}.read() - 511) / 511) * 100)`;
-      return [code, this.ORDER_FUNCTION_CALL];
-    },
-  },
-  {
     id: 'voltage',
     text: (
       <Text
@@ -156,6 +119,81 @@ export const blocks = (meta) => [
       this.definitions_[pinName] = `${pinName} = ADC(Pin(${pin}))`;
       this.definitions_[`${pinName}_atten`] = `${pinName}.atten(ADC.ATTN_11DB)`;
       const code = `round(${pinName}.read_uv() / 1_000_000, 2)`; // 精度，小数点后两位
+      return [code, this.ORDER_FUNCTION_CALL];
+    },
+  },
+  '---',
+  {
+    id: 'joystick',
+    text: (
+      <Text
+        id="blocks.ainput.joystick"
+        defaultMessage="pin [PIN] joystick X value"
+      />
+    ),
+    output: 'number',
+    inputs: {
+      PIN: meta.boardPins
+        ? {
+            menu: meta.boardPins.adc,
+          }
+        : {
+            type: 'positive_integer',
+            defaultValue: 1,
+          },
+    },
+    ino(block) {
+      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+      this.definitions_[`setup_pin_${pin}`] = `pinMode(${pin}, INPUT);`;
+      const code = `round(((analogRead(${pin}) - 511) / 511.0f) * 100)`;
+      return [code, this.ORDER_FUNCTION_CALL];
+    },
+    mpy(block) {
+      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+      const pinName = `pin_${pin}`;
+      this.definitions_['import_pin'] = 'from machine import Pin';
+      this.definitions_['import_adc'] = 'from machine import ADC';
+      this.definitions_[pinName] = `${pinName} = ADC(Pin(${pin}))`;
+      this.definitions_[`${pinName}_atten`] = `${pinName}.atten(ADC.ATTN_11DB)`;
+      this.definitions_[`${pinName}_width`] = `${pinName}.width(ADC.WIDTH_10BIT)`;
+      const code = `round(((${pinName}.read() - 511) / 511) * 100)`;
+      return [code, this.ORDER_FUNCTION_CALL];
+    },
+  },
+  {
+    id: 'joystickY',
+    text: (
+      <Text
+        id="blocks.ainput.joystickY"
+        defaultMessage="pin [PIN] joystick Y value"
+      />
+    ),
+    output: 'number',
+    inputs: {
+      PIN: meta.boardPins
+        ? {
+            menu: meta.boardPins.adc,
+          }
+        : {
+            type: 'positive_integer',
+            defaultValue: 1,
+          },
+    },
+    ino(block) {
+      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+      this.definitions_[`setup_pin_${pin}`] = `pinMode(${pin}, INPUT);`;
+      const code = `round(((analogRead(${pin}) - 511) / 511.0f) * 100)`;
+      return [code, this.ORDER_FUNCTION_CALL];
+    },
+    mpy(block) {
+      const pin = meta.boardPins ? block.getFieldValue('PIN') : this.valueToCode(block, 'PIN', this.ORDER_NONE);
+      const pinName = `pin_${pin}`;
+      this.definitions_['import_pin'] = 'from machine import Pin';
+      this.definitions_['import_adc'] = 'from machine import ADC';
+      this.definitions_[pinName] = `${pinName} = ADC(Pin(${pin}))`;
+      this.definitions_[`${pinName}_atten`] = `${pinName}.atten(ADC.ATTN_11DB)`;
+      this.definitions_[`${pinName}_width`] = `${pinName}.width(ADC.WIDTH_10BIT)`;
+      const code = `round(((${pinName}.read() - 511) / 511) * 100)`;
       return [code, this.ORDER_FUNCTION_CALL];
     },
   },
--- extensions/ainput/src/info.jsx
+++ extensions/ainput/src/info.jsx
@@ -8,29 +8,29 @@ export default {
   name: (
     <Text
       id="blocks.ainput.name"
-      defaultMessage="Simple Sensors"
+      defaultMessage="Simple Inputs"
     />
   ),
   description: (
     <Text
       id="blocks.ainput.description"
-      defaultMessage="Simple analog/digital sensors."
+      defaultMessage="Simple digital/analog/jokstick inputs or sensors."
     />
   ),
-  tags: ['device', 'arduino', 'sensor'],
+  tags: ['device', 'arduino', 'sensor', 'controller'],
 };
 
 addLocalesMessages({
   en: {
-    'blocks.ainput.name': 'Simple Sensors',
-    'blocks.ainput.description': 'Simple analog/digital sensors.',
+    'blocks.ainput.name': 'Simple Inputs',
+    'blocks.ainput.description': 'Simple digital/analog/jokstick inputs or sensors.',
   },
   'zh-Hans': {
-    'blocks.ainput.name': '简单传感器',
-    'blocks.ainput.description': '简单模拟/数字传感器。',
+    'blocks.ainput.name': '简单输入',
+    'blocks.ainput.description': '简单数字/模拟/摇杆输入和传感器。',
   },
   'zh-Hant': {
-    'blocks.ainput.name': '簡單模擬傳感器',
-    'blocks.ainput.description': '簡單模擬/數字傳感器。',
+    'blocks.ainput.name': '簡單輸入',
+    'blocks.ainput.description': '簡單數字/模擬/摇杆輸入和傳感器。',
   },
 });
